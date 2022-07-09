7ab15d9dca17c1b2b02f0b5d5f5af9b57417f0fd
Jeff Hodges <jeff@somethingsimilar.com>
2022-07-09 02:38:57 -0700

    quick broken version of BiscuitBuilder code

--- src/lib.rs
+++ src/lib.rs
@@ -1,10 +1,11 @@
 use pyo3::prelude::*;
 
-use biscuit_auth::{builder::*, error, Authorizer, Biscuit, KeyPair as BiscKeyPair};
+use biscuit_auth::builder::BiscuitBuilder as BABiscuitBuilder;
+use biscuit_auth::{builder::*, error, Biscuit as BABiscuit, KeyPair as BAKeyPair};
 
 #[pyclass]
 struct KeyPair {
-    key_pair: BiscKeyPair,
+    key_pair: BAKeyPair,
 }
 
 #[pymethods]
@@ -12,13 +13,36 @@ impl KeyPair {
     #[new]
     fn py_new() -> Self {
         return Self {
-            key_pair: BiscKeyPair::new(),
+            key_pair: BAKeyPair::new(),
         };
     }
+}
+
+#[pyclass]
+struct Biscuit {
+    biscuit: BABiscuit,
+}
+
+#[pymethods]
+impl Biscuit {
+    #[staticmethod]
+    fn builder(root: &KeyPair) -> PyResult<BiscuitBuilder> {
+        return Ok(BiscuitBuilder {
+            builder: BABiscuit::builder(root),
+        });
+    }
+}
+
+#[pyclass]
+struct BiscuitBuilder {
+    builder: BABiscuitBuilder,
 }
 
 #[pymodule]
 fn biscuit_auth(_py: Python, m: &PyModule) -> PyResult<()> {
     m.add_class::<KeyPair>()?;
+    m.add_class::<Biscuit>()?;
+    m.add_class::<BiscuitBuilder>()?;
+
     Ok(())
 }
