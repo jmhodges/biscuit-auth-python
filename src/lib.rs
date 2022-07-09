1b6ee958239abbb8f6e2fc1db149509b3fd17480
Jeff Hodges <jeff@somethingsimilar.com>
2022-07-09 13:38:35 -0700

    ouroborous magicks for lifetime annotation

--- src/lib.rs
+++ src/lib.rs
@@ -3,6 +3,7 @@ use pyo3::prelude::*;
 use biscuit_auth::builder::BiscuitBuilder as BABiscuitBuilder;
 use biscuit_auth::{builder::*, error, Biscuit as BABiscuit, KeyPair as BAKeyPair};
 
+use ouroboros::self_referencing;
 #[pyclass]
 struct KeyPair {
     key_pair: BAKeyPair,
@@ -26,16 +27,29 @@ struct Biscuit {
 #[pymethods]
 impl Biscuit {
     #[staticmethod]
-    fn builder(root: &KeyPair) -> PyResult<BiscuitBuilder> {
-        return Ok(BiscuitBuilder {
-            builder: BABiscuit::builder(root),
-        });
+    fn builder(py_root: &KeyPair) -> PyResult<BiscuitBuilder> {
+        let root = BAKeyPair::from(py_root.key_pair.private());
+        // BiscuitBuilderTryBuilder is made by oururoboros to allow store
+        // BABiscuitBuilder's in a Python object. BABiscuitBuilder has a
+        // required lifetime annotation, and so we adapt a technique from crfs
+        // to our own needs (see
+        // https://github.com/messense/crfs-rs/blob/main/python/src/lib.rs#L56-L72)
+        let builder_res = BiscuitBuilderTryBuilder {
+            key_pair: root,
+            builder_builder: |kp| Ok(BABiscuit::builder(kp)),
+        }
+        .try_build();
+        return builder_res;
     }
 }
 
 #[pyclass]
+#[self_referencing]
 struct BiscuitBuilder {
-    builder: BABiscuitBuilder,
+    key_pair: BAKeyPair,
+    #[borrows(key_pair)]
+    #[not_covariant]
+    builder: BABiscuitBuilder<'this>,
 }
 
 #[pymodule]
